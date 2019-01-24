003ceddfab0b59b1cbd88bcc534c99bbf9574cbe
Fabio <fabiozoroastro@gmail.com>
2019-01-24 14:21:24 -0800

    added CircleCI config.yml ( and deleted an unused class)

--- order/src/main/kotlin/zoret4/allaboutmoney/order/model/service/contracts/PaymentService.kt
+++
@@ -1,7 +0,0 @@
-package zoret4.allaboutmoney.order.model.service.contracts
-
-import zoret4.allaboutmoney.order.model.domain.Order
-
-interface PaymentService {
-    fun createPayment(order: Order): Order
-}
