003cbd9d0f92cb6c7c39bef0b4d2e9618c5913f9
Fabio <fabiozoroastro@gmail.com>
2019-02-04 15:07:29 -0800

    UNBROKEN: commented out some WIP work

--- order/src/test/kotlin/zoret4/allaboutmoney/order/model/domain/CustomerTest.kt
+++ order/src/test/kotlin/zoret4/allaboutmoney/order/model/domain/CustomerTest.kt
@@ -5,14 +5,14 @@ import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.Mockito.RETURNS_DEEP_STUBS
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ClassWithFinal.class)
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest(ClassWithFinal.class)
 class CustomerTest {
 
     @Test
     fun `customer is considered full if there is email`(){
-        val customer = Mockito.mock(Customer::class.java, RETURNS_DEEP_STUBS)
-        println(customer)
+//        val customer = Mockito.mock(Customer::class.java, RETURNS_DEEP_STUBS)
+//        println(customer)
     }
 
     fun `customer is NOT full when email is empty`(){
