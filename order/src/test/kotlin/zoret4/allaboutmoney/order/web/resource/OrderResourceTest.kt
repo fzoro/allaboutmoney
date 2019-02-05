a2f9d9b495cca82fc5e84a5070fb309ac3f532f1
Fabio <fabiozoroastro@gmail.com>
2019-02-04 16:02:39 -0800

    JUNIT5: junit 5 settled

--- order/src/test/kotlin/zoret4/allaboutmoney/order/web/resource/OrderResourceTest.kt
+++ order/src/test/kotlin/zoret4/allaboutmoney/order/web/resource/OrderResourceTest.kt
@@ -1,10 +1,10 @@
 package zoret4.allaboutmoney.order.web.resource
 
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertNotNull
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import zoret4.allaboutmoney.order.model.domain.Address
 import zoret4.allaboutmoney.order.model.domain.Customer
 import zoret4.allaboutmoney.order.model.domain.TaxonomyType
@@ -21,7 +21,7 @@ import zoret4.allaboutmoney.order.model.service.contracts.CustomerService
 import zoret4.allaboutmoney.order.util.TestsHelper.Companion.any
 import java.time.LocalDate
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderResourceTest {
 
@@ -33,8 +33,7 @@ class OrderResourceTest {
 
     private val baseUri = "/customers"
 
-
-    @Before
+    @BeforeEach
     fun setup() {
         MockitoAnnotations.initMocks(this)
     }
