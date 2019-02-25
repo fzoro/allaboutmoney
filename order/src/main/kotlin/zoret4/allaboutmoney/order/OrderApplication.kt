ba5a551b847cc1823356c822ec50e02498e80e75
Fabio <fabiozoroastro@gmail.com>
2019-02-25 15:58:05 -0800

    wip: added Mixin to ObjectMapper @Bean so Customer.moipAccount NULL does not prevent serialization

--- order/src/main/kotlin/zoret4/allaboutmoney/order/OrderApplication.kt
+++ order/src/main/kotlin/zoret4/allaboutmoney/order/OrderApplication.kt
@@ -1,5 +1,7 @@
 package zoret4.allaboutmoney.order
 
+import br.com.moip.resource.Customer
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -9,19 +11,27 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import zoret4.allaboutmoney.order.configuration.props.AppProperties
 
+
 @EnableConfigurationProperties(AppProperties::class)
 @SpringBootApplication
 class OrderApplication
 
 fun main(args: Array<String>) {
+
     runApplication<OrderApplication>(*args)
 }
 
 @Bean
 fun objectMapper(): ObjectMapper {
+    abstract class Mixin {
+        @JsonIgnore
+        abstract fun getMoipAccountId(): String
+    }
+
     val objectMapper = ObjectMapper()
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    objectMapper.addMixIn(Customer::class.java, Mixin::class.java)
     return objectMapper
 
 
