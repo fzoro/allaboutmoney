12355481b20c0fb45b8642a1969eed01ef44ab66
Fabio <fabiozoroastro@gmail.com>
2019-05-16 15:31:35 -0700

    wip: wirecard webhook events

--- order/src/main/kotlin/zoret4/allaboutmoney/order/web/vendor/resource/WirecardWebhookEventResource.kt
+++ order/src/main/kotlin/zoret4/allaboutmoney/order/web/vendor/resource/WirecardWebhookEventResource.kt
@@ -1,10 +1,28 @@
 package zoret4.allaboutmoney.order.web.vendor.resource
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import zoret4.allaboutmoney.order.configuration.props.AppProperties
+import zoret4.allaboutmoney.order.model.domain.Order
+import zoret4.allaboutmoney.order.model.vendor.wirecard.service.WirecardOrderService
 
 
 @RestController
-@RequestMapping("/wirecard/webhooks")
-class WirecardWebhookEventResource
+@RequestMapping("/wirecard/events")
+class WirecardWebhookEventResource(
+        private val wirecardOrderService: WirecardOrderService) {
+
+
+    @PostMapping("/payment")
+    @ResponseStatus(HttpStatus.OK)
+    fun capturePaymentEvent(@RequestBody body: Order) {
+
+    }
+
+    @PostMapping("/order")
+    @ResponseStatus(HttpStatus.OK)
+    fun captureOrderEvent(@RequestBody body: Order,
+                          @RequestHeader("Authorization") authToken: String) {
+
+    }
+}
